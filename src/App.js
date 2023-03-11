5ec469e34d998faa489292bea88d63f9cbbb1a29
Dasha <da6a_88@abv.bg>
2023-03-11 12:59:45 +0200

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
+import { useEffect, useState } from 'react';
+import * as userService from './services/userService';
 import { Fragment } from 'react';
 
 import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Search } from './components/Search';
 import './App.css';
+import { UserList } from './components/UserList';
 
 function App() {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    userService.getAll()
+    .then(setUser)
+    .catch(err => {
+      console.log('Error' + err);
+    });
+  }, []);
+
+  const onUserCreate = async(e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const createdUser = await userService.create(data);
+    setUser(state => [createdUser, ...state]);
+  };
+
   return (
     <Fragment>
       <Header />
       <main className="main">
         <section className="card users-container">
+
           <Search />
+          <UserList users={users} onUserCreate={onUserCreate} />
+
         </section>
 
       </main>
---
+++ src/services/userService.js
@@ -0,0 +1,34 @@
+const baseUrl = 'http://localhost:3005/api/users';
+
+export const getAll = async () => {
+    const response = await fetch(baseUrl);
+    const result = await response.json();
+
+    return result.users;
+};
+
+export const getOne = async (userId) => {
+    const response = await fetch(`${baseUrl}/${userId}`);
+    const result = await response.json();
+
+    return result.user;
+};
+
+export const create = async (userData) => {
+    const { country, city, street, streetNumber, ...data } = userData;
+    data.address = {
+        country, 
+        city, 
+        street, 
+        streetNumber,
+    };
+    const responce = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    const result = await responce.json();
+    return result;
+};
